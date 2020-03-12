2f922af1a92e53ae57f030d562c75e51c3202d52
陈俊江 <https://gitee.com/chenjunjiang>
2020-03-12 08:57:08 +0800

    定时器和断续器

---
+++ src/basic/time/time.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/**
+定时器永远不需要向它的C字段发送第二个元素值。这是因为，定时器一旦到期就意味着在我们重置它之前它无法被再次使用。注意，如果
+我们在定时器到期之前停止了它，那么该定时器的字段C也就没有任何缓冲任何元素值了，这之后再去试图从它的C字段中接收元素是不会
+有任何结果的。更重要的是，这样做还会使当前Goroutine永远被阻塞。再次强调，重置已被停止的定时器是使它恢复如初的唯一方法。
+*/
+func main() {
+	/*// 初始化一个到期时间距此时的间隔为2秒的定时器
+	t := time.NewTimer(2 * time.Second)
+	now := time.Now()
+	fmt.Printf("Now time: %v.\n", now)
+	// 到期事件通过C这个缓冲通道传达的，一旦触及到期时间，定时器就会向自己的C字段发送一个time.Time类型的元素值
+	// 这个元素值代表了该定时器的绝对到期时间
+	expire := <-t.C
+	fmt.Printf("Expiration time: %v.\n", expire)
+
+	//和上面的效果一样
+	expire = <-time.After(2 * time.Second)
+	fmt.Printf("Expiration time: %v.\n", expire)*/
+
+	/*var t *time.Timer
+	f := func() {
+		fmt.Printf("Expiration time: %v.\n", time.Now())
+		fmt.Printf("C's len: %d\n", len(t.C))
+		// 重置之后定时器又恢复执行
+		t.Reset(1*time.Second)
+	}
+	t = time.AfterFunc(1*time.Second, f)
+	time.Sleep(200 * time.Second)*/
+
+	/**
+	断续器,周期性的传达到期事件,定时器在被重置之前只会传达一次到期事件，而断续器会持续工作直到被停止
+	*/
+	sign := make(chan byte, 1)
+	var ticker *time.Ticker = time.NewTicker(10 * time.Second)
+	ticks := ticker.C
+	go func() {
+		for _ = range ticks {
+			i := 1
+			for {
+				fmt.Println("test......" + strconv.Itoa(i))
+				i++
+				if i > 10 {
+					break
+				}
+			}
+			ticker.Stop()
+			/**
+			close函数是一个内建函数， 用来关闭channel，这个channel要么是双向的， 要么是只写的（chan<- Type）。
+			这个方法应该只由发送者调用， 而不是接收者。
+			当最后一个发送的值都被接收者从关闭的channel(下简称为c)中接收时,
+			接下来所有接收的值都会非阻塞直接成功，返回channel元素的零值。
+			如下的代码：
+			如果c已经关闭（c中所有值都被接收）， x, ok := <- c， 读取ok将会得到false。
+			*/
+			// 关闭只读channel会报编译错误，ticks是只读通道
+			// close(ticks)
+			fmt.Println("stop ticker")
+			break
+		}
+		sign <- 1
+	}()
+	<-sign
+}
--- src/basic/unbufferedchannel/unbufchannel.go
+++ src/basic/unbufferedchannel/unbufchannel.go
@@ -12,10 +12,13 @@ import (
 3、针对非缓冲通道的接收操作会在与之对应的发送操作完成之前完成。
 在缓冲通道中，由于元素值的传递是异步的，所以发送操作在成功向通道发送元素值之后就会立即结束。然而，针对非缓冲通道的操作在这方面的表现正好相反。发送操作
 向非缓冲通道发送元素值的时候，会等待能够接收该元素值的那个接收操作。并且，只有确保该元素值被成功接收，它才会真正的完成执行。
+总之，非缓冲通道是无法缓冲任何元素值的。因此，针对它们的收发操作能否立即执行完全取决于当时是否有可配对的操作。
+如果在发送端每次都需要确保元素值已被接收的情况下，那么使用非缓冲通道是合适的。否则，我们应该选用缓冲通道来实现相关流程
+的异步化，并以此提高整个程序的性能。
 */
 func main() {
 	// 可以利用非缓冲通道的特性来实现多个Goroutine之间的同步
-	unbufChan := make(chan int)
+	/*unbufChan := make(chan int)
 	go func() {
 		fmt.Println("Sleep a second...")
 		time.Sleep(time.Second)
@@ -26,5 +29,39 @@ func main() {
 	num := 1
 	fmt.Printf("Send integer %d...\n", num)
 	unbufChan <- num
-	fmt.Println("Done.")
+	fmt.Println("Done.")*/
+
+	unbufChan := make(chan int)
+	sign := make(chan byte, 2)
+	go func() {
+		for i := 0; i < 10; i++ {
+			select {
+			case unbufChan <- i:
+			case unbufChan <- i + 10:
+			default:
+				fmt.Println("default!")
+			}
+			time.Sleep(time.Second)
+		}
+		close(unbufChan)
+		fmt.Println("The channel is closed.")
+		sign <- 0
+	}()
+	go func() {
+	loop:
+		for {
+			select {
+			case e, ok := <-unbufChan:
+				if !ok {
+					fmt.Println("Closed channel.")
+					break loop
+				}
+				fmt.Printf("e: %d\n", e)
+				time.Sleep(2 * time.Second)
+			}
+		}
+		sign <- 1
+	}()
+	<-sign
+	<-sign
 }
