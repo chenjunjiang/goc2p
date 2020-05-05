ab0d771fd587afec61097877c6fa8271b93ee371
chenjunjiang <chenjunjiang1988@126.com>
2020-05-05 17:10:08 +0800

    反射说明修改

--- src/basic/reflect/reflect.go
+++ src/basic/reflect/reflect.go
@@ -102,13 +102,12 @@ func main() {
 	当执行reflect.ValueOf(interface{})之后，就得到了一个类型为”reflect.Value”变量，可以通过它本身的Interface()方法获得接口变量的真实内容，
 	然后可以通过类型判断进行转换，转换为原有真实类型。不过，我们可能是已知原有类型，也有可能是未知原有类型，因此，下面分两种情况进行说明。
 	*/
-	// 已知类型后转换为其对应的类型的做法如下，直接通过Interface方法然后强制转换:realValue := value.Interface().(已知的类型)
+	// 已知类型后转换为其对应的类型的做法如下，直接通过Interface()方法然后强制转换:realValue := value.Interface().(已知的类型)
 	pointer := reflect.ValueOf(&num)
 	value := reflect.ValueOf(num)
 	/**
 	可以理解为“强制转换”，但是需要注意的时候，转换的时候，如果转换的类型不完全符合，则直接panic;Golang 对类型要求非常严格，类型一定要完全符合
-	转换的时候，要区分是指针还是值
-	反射可以将“反射类型对象”再重新转换为“接口类型变量”
+	转换的时候，要区分是指针还是值。反射可以将“反射类型对象”再重新转换为“接口类型变量”
 	*/
 	convertPointer := pointer.Interface().(*float64)
 	convertValue := value.Interface().(float64)
